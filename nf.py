fb48648826f79282431d3082bd7d007e975b6772
David Bacon <DBacon89@hotmail.com>
2022-02-08 11:56:35 -0500

    main file for api

---
+++ nf.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_restful import fields, marshal_with
+
+import nf_core
+
+app = Flask(__name__)
+api = Api(app)
+
+# get the parameters to the nextflow list function
+
+resource_fields = {
+    'filter_by':   fields.List,
+    'sort_by':    fields.String,
+    'as_json': fields.Boolean,
+}
+
+class NextflowWorkflows(Resource):
+    @marshal_with(resource_fields)
+    def get(self,  **kwargs):
+         # do some sanity checks here
+        data = {'filter_by': kwargs['filter_by']} 
+        return {workflows: nf_core.list.list_workflows(filter_by=data['filter_by'])}
+
+
+
+api.add_resource(NextflowWorkflows, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
