95fcc9dd7db6a983fbc1788361fe1cb8a0b7825a
David Bacon <DBacon89@hotmail.com>
2022-02-09 12:07:31 -0500

    incorporating Docker

--- nf.py
+++ nf.py
@@ -27,5 +27,5 @@ class NextflowWorkflows(Resource):
 api.add_resource(NextflowWorkflows, '/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host = '0.0.0.0',port = 5005)
 
